6cbcaddcd33818ac844291ead9d95380dc107b0f
Daniel Kim <danielckim05@gmail.com>
2015-09-21 10:44:22 -0400

    Improved SeverHandler

--- app/src/main/java/com/example/danielkim/structure_generator/ServerHandler.java
+++ app/src/main/java/com/example/danielkim/structure_generator/ServerHandler.java
@@ -1,7 +1,5 @@
 package com.example.danielkim.structure_generator;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 
 import org.apache.http.HttpEntity;
@@ -13,11 +11,9 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -26,22 +22,18 @@ public class ServerHandler {
 
     public static final String SERVER_ADDRESS = "http://dk.comxa.com/";
     public static final int CONNECTION_TIMEOUT = 1000 * 15;
-
-
-//    ProgressDialog progressDialog;
-//    public ServerHandler(Context context) {
-//        progressDialog = new ProgressDialog(context);
-//        progressDialog.setTitle("Processing");
-//        progressDialog.setMessage("Please wait...");
-//        progressDialog.setCancelable(false);
-//    }
+    User user;
+    Callback callback;
 
     public void registerToDatabase(User user) {
         new RegisterUserAsyncTask(user).execute();
     }
 
-    public void getUserFromDB(User user) {
-        new GetUserAsyncTask(user).execute();
+    public void getUserFromDB(User user, Callback callback) {
+        this.user = user;
+        this.callback = callback;
+        new GetUserAsyncTask().execute(user);
+        //return user;
     }
     public class RegisterUserAsyncTask extends AsyncTask <Void, Void, Void> {
         User user;
@@ -78,16 +70,13 @@ public class ServerHandler {
         }
     }
 
-    public class GetUserAsyncTask extends AsyncTask<Void, Void, User> {
-        User user;
-        User returnedUser = null;
-
-        public GetUserAsyncTask(User user) {
-            this.user = user;
-        }
+    public class GetUserAsyncTask extends AsyncTask<User, Void, User> {
 
         @Override
-        protected User doInBackground(Void... params) {
+        protected User doInBackground(User... users) {
+            User returnedUser = null;
+            User user = users[0];
+
             ArrayList<NameValuePair> dataToSend = new ArrayList<>();
             dataToSend.add(new BasicNameValuePair("email", user.email));
             dataToSend.add(new BasicNameValuePair("password", user.password));
@@ -102,28 +91,41 @@ public class ServerHandler {
                 //Executing HttpPost gets a response; Store response in httpResponse
                 HttpResponse httpResponse = client.execute(post);
 
-                //Data from httpReponse
+                //Data from httpResponse
                 HttpEntity entity = httpResponse.getEntity();
+                System.out.println("entity content is: " + entity);
                 String result = EntityUtils.toString(entity);
-                System.out.println("result = " + result);
-                //Read from JSON file and store in jObject
-                //JSONArray jArray = new JSONArray(result);
-                JSONObject jObject = new JSONObject(result);
-                if (jObject.length() == 0) {
-                    returnedUser = null;
-                } else {
+                System.out.println(result);
 
-//                    String fname = jObject.getJSONArray(1).toString();
-//                    String lname = jObject.getJSONArray(2).toString();
-//                    String org = jObject.getJSONArray(4).toString();
-//                    String confirmPass = jObject.getJSONArray(6).toString();
+                //Read from JSON file and store in jObject
+                JSONObject jObject = new JSONObject(result);
+                if (jObject.length() ==0) {
+                    System.out.println("empty result");
+                    return returnedUser;
+                } else {
+                    System.out.println("result is not empty and the result is: " + result);
+
+                //JSONObject userDetails = jObject.getJSONObject("result");
+//                if (jObject.length() == 0) { //(jObject.length() == 0) {
+//                    returnedUser = null;
+//                    System.out.println("jObject is: " +jObject);
+//                } else {
+
+//                    String first_name  = jArray.getString(0);
+//                    System.out.println(first_name);
+//                    String last_name = jArray.getString(1).toString();
+//                    String organization = jArray.getString(3).toString();
+//                    String confirm_password = jArray.getString(5).toString();
 
                     String first_name = jObject.getString("first_name");
                     String last_name = jObject.getString("last_name");
-                    String organization = jObject.getString("Organization");
+                    String organization = jObject.getString("organization");
                     String confirm_password = jObject.getString("confirm_password");
 
+
                     returnedUser = new User(first_name, last_name, user.email, organization, user.password, confirm_password);
+                    System.out.println("printing user : " + first_name + " " + last_name + " " + organization + " " + confirm_password);
+                    System.out.println(returnedUser.fname + " " + returnedUser.lname);
                 }
 
             } catch (Exception e){
@@ -131,7 +133,15 @@ public class ServerHandler {
             }
             return returnedUser;
         }
+
+        @Override
+        protected void onPostExecute(User returnedUser) {
+            System.out.println("XXXXXXXXX: " + user.email);
+            System.out.println("XXXXXXXXX: " + returnedUser);
+            callback.getCallback(returnedUser);
+        }
     }
+
 
     //Setup for http: set connection and timeout
     private HttpParams getHttpRequestParams() {

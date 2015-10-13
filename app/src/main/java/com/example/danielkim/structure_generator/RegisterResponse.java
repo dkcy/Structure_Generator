e9e450d80a499bfac601cbb9fc548d9fc6964b79
Daniel Kim <danielckim05@gmail.com>
2015-10-13 18:41:15 -0400

    Callback for checking existing email

---
+++ app/src/main/java/com/example/danielkim/structure_generator/RegisterResponse.java
@@ -0,0 +1,8 @@
+package com.example.danielkim.structure_generator;
+
+/**
+ * Created by danielkim on 10/6/15.
+ */
+public interface RegisterResponse {
+    void getRegisterResponse(String num);
+}
